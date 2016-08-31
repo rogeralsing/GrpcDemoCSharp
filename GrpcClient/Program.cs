060687e58db388b321acc2e75135bde9f4ea1f74
rogeralsing <rogeralsing@gmail.com>
2016-08-31 09:54:43 +0200

    demo

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(new HelloRequest { Name = "Roger" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcContracts/build.bat
@@ -0,0 +1,1 @@
+﻿protoc Protos.proto -I. --csharp_out=.   --csharp_opt=file_extension=.g.cs --grpc_out . --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
